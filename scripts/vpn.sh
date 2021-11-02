c72998caa201ab251e3b5f4085705c9f3a61da1e
Alex Bates <alexlbates@gmail.com>
2021-11-02 18:34:27 -0400

    added anyconnect + windows handling to vpn script

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 password=`cat ~/.work_password`
-echo "$password" | sudo openconnect --user=alex.bates --passwd-on-stdin --authgroup=VPN-RA ssl.clearcaptions.com
+
+# openconnect version 
+#echo "$password" | sudo openconnect --user=alex.bates --passwd-on-stdin --authgroup=VPN-RA ssl.clearcaptions.com
+
+
+# anyconnect version
+exe_path="/mnt/c/Program Files (x86)/Cisco/Cisco AnyConnect Secure Mobility Client/vpncli.exe"
+command_string="connect ssl.clearcaptions.com\n1\nalex.bates\n${password}"
+
+vpn_state=`"${exe_path}" state`
+
+if [[ "$vpn_state" == *"Disconnected"* ]] ;then
+    echo "VPN Not Connected. Connecting now... "
+    echo -e $command_string | "${exe_path}" -s
+else 
+    echo "VPN Connected. Disconnecting now... "
+    "${exe_path}" disconnect
+fi
+
+
