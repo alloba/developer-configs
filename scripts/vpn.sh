f9e4fe94e1d2748261edf7eed9fb628a5fa0ea8b
Alex Bates <alexlbates@gmail.com>
2021-11-04 09:53:02 -0400

    update vpn script to accept flags

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 password=`cat ~/.work_password`
 
 # openconnect version 
@@ -9,14 +10,29 @@ password=`cat ~/.work_password`
 exe_path="/mnt/c/Program Files (x86)/Cisco/Cisco AnyConnect Secure Mobility Client/vpncli.exe"
 command_string="connect ssl.clearcaptions.com\n1\nalex.bates\n${password}"
 
-vpn_state=`"${exe_path}" state`
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    echo "vpn.sh -c to connect"
+    echo "       -d to disconnect"
+    exit
+fi
+
+
+vpn_state=$("${exe_path}" state)
 
 if [[ "$vpn_state" == *"Disconnected"* ]] ;then
-    echo "VPN Not Connected. Connecting now... "
-    echo -e $command_string | "${exe_path}" -s
-else 
-    echo "VPN Connected. Disconnecting now... "
+    vpn_connected=false
+else
+    vpn_connected=true
+fi
+
+if [[ "$1" == "-d" ]] && [[ $vpn_connected == true ]]; then
+    echo "Disconnecting VPN..."
     "${exe_path}" disconnect
+elif [ "$1" == "-c" ] && [ $vpn_connected == false ]; then
+    echo "Connecting to VPN..."
+    echo -e "$command_string" | "${exe_path}" -s
+else
+    echo "No action taken. VPN Connected: ${vpn_connected}"
 fi
 
 
