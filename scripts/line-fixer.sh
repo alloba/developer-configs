6fc98d17f6b1c1a6f2e440ad41d8fedf5bd994ec
Alex Bates <alexlbates@gmail.com>
2022-03-19 00:00:30 -0400

    - Added new script to quickly fix new line characters in files. - Fixed all files in the script folder of this project with new script.

---
+++ scripts/line-fixer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Automatically convert all files in a directory 
+# To Unix-friendly line endings. 
+# Apprently this is going to be an issue in my life now, 
+# since git and WSL feel like getting fresh with me lately... 
+#
+# Requies dos-2-unix be installed. 
+
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+    echo "line-fixer.sh {folder_path} to convert all in folder."
+    echo "  Leave path blank to convert current directory."
+    exit
+fi
+
+if [[ $# -eq 0 ]]; then 
+    find .    -type f -exec dos2unix {} \;
+else 
+    find "${1}" -type f -exec dos2unix {} \;
+fi 
+
