1fd1ae7787f0fdaa03216be2ce9cbcc8fc582147
Alex Bates <alexlbates@gmail.com>
2022-11-17 17:02:02 -0500

    fix suggestion selection for non-primary computers in nvim

--- .config/nvim/lua/plugins/config/coc.lua
+++ .config/nvim/lua/plugins/config/coc.lua
@@ -8,11 +8,14 @@ vim.api.nvim_set_keymap('i', '<Tab>', 'coc#pum#visible() ? coc#pum#next(1) : "<T
 vim.api.nvim_set_keymap('i', '<S-Tab>', 'coc#pum#visible() ? coc#pum#prev(1) : "<S-Tab>"',
     { noremap = true, expr = true })
 
+-- my never ending attempts to get the enter key to select suggestions.
+-- i've now given up entirely and fallen back to vimscript
+vim.cmd [[
+" Make <CR> to accept selected completion item or notify coc.nvim to format
+" <C-g>u breaks current undo, please make your own choice.
+inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
+                              \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+                              ]]
 
--- Enter to confirm suggestion in popup
--- <C-g>u fucks undo a bit, but I've decided to not care. 
-vim.api.nvim_set_keymap('i', '<CR>', 'coc#pum#visible() ? coc#pum#confirm() : "<C-g>u<CR><c-r>=coc#on_enter()<CR>"',
-    { noremap = true, expr = true, silent = true })
-
-vim.g.coc_global_extensions = {'coc-prettier', 'coc-sumneko-lua', 'coc-python', 'coc-json'}
---{'coc-prettier', 'coc-sumneko-lua', 'coc-html', 'coc-tsserver', 'coc-python', 'coc-json', 'coc-go'}
+-- Always install these extensions. 
+vim.g.coc_global_extensions = { 'coc-prettier', 'coc-sumneko-lua', 'coc-python', 'coc-json' }
