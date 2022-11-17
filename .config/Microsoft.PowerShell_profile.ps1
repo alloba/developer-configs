4cf2f94a3a3b4e1af36a3950917948ca8ec18dce
Alex Bates <alexlbates@gmail.com>
2022-11-17 11:20:59 -0500

    working example of a powershell profile added

---
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+# This is more meant to be a working example than a fully reproducible config file. 
+# Care should be taken to place it in the correct place (~/.config is not correct) 
+#
+# Setting environment variables in this file is not really standard behavior for windows, 
+# setting XDG values here is just a workaround for applications that cannot handle 
+# the default changing on windows (thinking specifically of Godot games). 
+
+$env:XDG_CONFIG_HOME = 'C:\Users\alexl\.config'
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\negligible.omp.json" | Invoke-Expression
+function journal {
+	python $HOME/projects/journal-assist/notes.py $HOME/projects/obsidian/ $HOME/projects/obsidian/journal/
+}
