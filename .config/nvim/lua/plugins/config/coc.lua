6278eede2643d3a023b0ab9450ef15c4badbdcc2
Alex Bates <alexlbates@gmail.com>
2022-11-18 14:49:44 -0500

    format document shortcut added to nvim

--- .config/nvim/lua/plugins/config/coc.lua
+++ .config/nvim/lua/plugins/config/coc.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>CocCommand<cr>',
-    { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>CocCommand<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>call CocAction("format")<cr>',    { noremap = true })
 
 -- Tab and shift-tab to move around the auto-suggestion popup
 vim.api.nvim_set_keymap('i', '<Tab>', 'coc#pum#visible() ? coc#pum#next(1) : "<Tab>"'
@@ -7,6 +7,7 @@ vim.api.nvim_set_keymap('i', '<Tab>', 'coc#pum#visible() ? coc#pum#next(1) : "<T
 
 vim.api.nvim_set_keymap('i', '<S-Tab>', 'coc#pum#visible() ? coc#pum#prev(1) : "<S-Tab>"',
     { noremap = true, expr = true })
+
 
 -- my never ending attempts to get the enter key to select suggestions.
 -- i've now given up entirely and fallen back to vimscript
@@ -17,5 +18,5 @@ inoremap <silent><expr> <CR> coc#pum#visible() ? coc#pum#confirm()
                               \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
                               ]]
 
--- Always install these extensions. 
+-- Always install these extensions.
 vim.g.coc_global_extensions = { 'coc-prettier', 'coc-sumneko-lua', 'coc-python', 'coc-json' }
