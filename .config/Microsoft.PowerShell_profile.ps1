c9779155c55261c86ba29ebc153faafb77b0105c
Alex Bates <alexlbates@gmail.com>
2022-11-29 12:12:10 -0500

    add message to powershell config pointing to default location it should be placed

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 # This is more meant to be a working example than a fully reproducible config file. 
 # Care should be taken to place it in the correct place (~/.config is not correct) 
+# (The default location is ~/OneDrive/Documents/Powershell)
 #
 # Setting environment variables in this file is not really standard behavior for windows, 
 # setting XDG values here is just a workaround for applications that cannot handle 
