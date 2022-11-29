452fbbd341dfd3e714797c8514c061567c85490b
Alex Bates <alexlbates@gmail.com>
2022-11-29 12:19:18 -0500

    add shortcut for emacs

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -6,8 +6,15 @@
 # setting XDG values here is just a workaround for applications that cannot handle 
 # the default changing on windows (thinking specifically of Godot games). 
 
+
+
 $env:XDG_CONFIG_HOME = 'C:\Users\alexl\.config'
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\negligible.omp.json" | Invoke-Expression
+
+function emacs {
+ & 'C:\Program Files\Emacs\emacs-28.2\bin\emacs.exe' --no-window-system 
+}
+
 function journal {
 	python $HOME/projects/journal-assist/notes.py $HOME/projects/obsidian/ $HOME/projects/obsidian/journal/
 }
