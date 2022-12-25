15fbcf2945134334e3bbc16ec474ccada8fd8655
Alex Bates <alexlbates@gmail.com>
2022-12-25 18:42:35 -0500

    more work on script thing. more doubts about script thing

--- .config/nvim/lua/planner-helper.lua
+++ .config/nvim/lua/planner-helper.lua
@@ -29,18 +29,15 @@ local function print_array(arr)
     end
 end
 
+local function is_completed_list_item(line)
+    local trimline = (line:gsub("^%s*(.-)%s*$", "%1"))
+    if starts_with(trimline, '[x]') or starts_with(trimline, '[X]') then
+        return true
+    end
+    return false
+end
+
 local function cycle_planner()
-    -- on the current buffer,
-    -- find the lines that make up the current day's events
-    -- copy it all down to the archive section
-    -- recreate the lines, removing the items marked as completed
-    -- put the new day's todo list at the top of the file.
-
-    -- get buffer 0 (current), from start to finish, ignore oob errors.
-    -- WARNING: lua indexes arrays starting at 1, not 0.
-    local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
-
-
     local HABITUAL_MARKER = '## Habitual'
     local TODO_MARKER = '## To Do'
     local SUBSECTION_END_MARKER = '#' -- heavily assuming that i will never change the format of these things to have subsections inside of them.
@@ -51,20 +48,27 @@ local function cycle_planner()
     local endindex_habitual = -1
     local startindex_todo = -1
     local endindex_todo = -1
-
     local startindex_archive = -1
+
+    local habitual_slice = nil
+    local todo_slice = nil
+
+    local content = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_buf_line_count(0), false)
 
     startindex_habitual = find_startswith_in_array(HABITUAL_MARKER, content)
     startindex_todo = find_startswith_in_array(TODO_MARKER, content)
     startindex_archive = find_startswith_in_array(ARCHIVE_MARKER, content)
 
-    local habitual_slice = trim_array(startindex_habitual, #content, content)
-    local todo_slice = trim_array(startindex_todo, #content, content)
+    habitual_slice = trim_array(startindex_habitual, #content, content)
+    todo_slice = trim_array(startindex_todo, #content, content)
 
     endindex_habitual = find_startswith_in_array(SUBSECTION_END_MARKER, trim_array(2, #habitual_slice, habitual_slice))
     endindex_todo = find_startswith_in_array(SUBSECTION_END_MARKER, trim_array(2, #todo_slice, todo_slice))
 
-    -- validate all indexes make sense. if something is wrong then dont change anything.
+    habitual_slice = trim_array(1, endindex_habitual, habitual_slice)
+    todo_slice = trim_array(1, endindex_todo, todo_slice)
+
+    -- validate all values make sense. if something is wrong then dont change anything.
     if startindex_habitual == -1 then print('could not find habitual startindex') return end
     if startindex_todo == -1 then print('could not find todo startindex') return end
 
@@ -72,12 +76,11 @@ local function cycle_planner()
     if endindex_todo == -1 then print('could not find todo startindex') return end
     if startindex_archive == -1 then print('could not find archive startindex') return end
 
+    if habitual_slice == nil then print('no value assigned for habitual slice') return end
+    if todo_slice == nil then print('no value assigned for todo slice') return end
 
 
-    --TODO get current date
-    --TODO create new top line items
-    --TODO insert new top line items (from previous step)
-    --TODO shift everything down to archive
+    -- go through each slice, and filter out all the list items that are completed.
 
 
 end
