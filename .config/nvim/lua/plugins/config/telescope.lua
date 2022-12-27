945a699dfafd6d0d81120ce1f25e0dbab7570dfe
Alex Bates <alexlbates@gmail.com>
2022-12-27 11:55:08 -0500

    add shortcut for nvim telescope spell suggest

--- .config/nvim/lua/plugins/config/telescope.lua
+++ .config/nvim/lua/plugins/config/telescope.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
-
+vim.api.nvim_set_keymap('n', '<leader>.', '<cmd>Telescope spell_suggest<cr>', {noremap = true})
 
 
