b8f1d1294085c11e60a64e1dbadd12103ca0d505
Alex Bates <alexlbates@gmail.com>
2023-01-31 10:23:36 -0500

    remove overlooked test file. move setting into coc.lua (autocomplete)

--- .config/nvim/after/plugin/coc.lua
+++ .config/nvim/after/plugin/coc.lua
@@ -44,6 +44,13 @@ vim.api.nvim_set_keymap(
     { noremap = true, silent = true, expr = true }
 )
 
+
+vim.keymap.set(
+    "i", "<cr>",
+    [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
+    { silent = true, noremap = true, expr = true, replace_keycodes = false }
+)
+
 --vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
 --    { silent = true, noremap = true, expr = true, replace_keycodes = false }
 --)
--- .config/nvim/after/plugin/test.lua
+++
@@ -1,4 +0,0 @@
-
-vim.keymap.set("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],
-    { silent = true, noremap = true, expr = true, replace_keycodes = false }
-)
