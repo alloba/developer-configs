a8e8fcb0eb1cce2ad602e16cec11aa1e8ef00d4b
Alex Bates <Alex.Bates@nielseniq.com>
2023-06-16 12:01:51 -0400

    increase default width for nvimtree

--- .config/nvim/after/plugin/nvimtree.lua
+++ .config/nvim/after/plugin/nvimtree.lua
@@ -1,5 +1,9 @@
 local has, tree = pcall(require, 'nvim-tree')
 if has then
     vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', { noremap = true })
-    tree.setup({})
+    tree.setup({
+        view = {
+            width = 70
+        }
+    })
 end
