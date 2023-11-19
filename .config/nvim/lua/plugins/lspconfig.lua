2e595beaf8dd269342d8cb5c29531a36149d9f49
Alex Bates <Alex.Bates@nielseniq.com>
2023-11-19 12:51:19 -0500

    replace deprecated function call in lspconfig

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -8,7 +8,7 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-    { "folke/neodev.nvim",  opts = {} },
+    { "folke/neodev.nvim", opts = {} },
     "mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     {
@@ -101,7 +101,7 @@ return {
       require("neoconf").setup(require("lazy.core.plugin").values(plugin, "opts", false))
     end
     -- setup autoformat
-    require("lazyvim.plugins.lsp.format").setup(opts)
+    require("lazyvim.util").format.setup(opts)
     -- setup formatting and keymaps
     Util.on_attach(function(client, buffer)
       require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
@@ -137,14 +137,14 @@ return {
 
     if type(opts.diagnostics.virtual_text) == "table" and opts.diagnostics.virtual_text.prefix == "icons" then
       opts.diagnostics.virtual_text.prefix = vim.fn.has("nvim-0.10.0") == 0 and "●"
-          or function(diagnostic)
-            local icons = require("lazyvim.config").icons.diagnostics
-            for d, icon in pairs(icons) do
-              if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
-                return icon
-              end
+        or function(diagnostic)
+          local icons = require("lazyvim.config").icons.diagnostics
+          for d, icon in pairs(icons) do
+            if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+              return icon
             end
           end
+        end
     end
 
     vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
