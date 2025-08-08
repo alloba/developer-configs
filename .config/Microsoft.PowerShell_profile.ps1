b8c41914879bcfb683b9b0e9416f5de9e7f5c876
Alex Bates <alexlbates@gmail.com>
2025-08-07 20:58:13 -0400

    more fiddling with windows settings

--- .config/Microsoft.PowerShell_profile.ps1
+++ .config/Microsoft.PowerShell_profile.ps1
@@ -3,8 +3,7 @@
 # (The default location is ~/OneDrive/Documents/Powershell)
 # just run `echo $PROFILE` to see where powershell wants it. 
 
-# This has barely ever helped me, and has caused a subtle bug in a game. So not changing it. 
-# $env:XDG_CONFIG_HOME = 'C:\Users\alexl\.config'
+$env:XDG_CONFIG_HOME = "${HOME}\.config"
 
 $PSStyle.FileInfo.Directory = "`e[36m"
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\dracula.omp.json" | Invoke-Expression
